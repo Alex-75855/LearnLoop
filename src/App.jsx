f613d777ff73ef14f238f06666f27dcd3e1e43b5
Alex-75855 <Alexander70425@gmail.com>
2025-11-15 18:14:35 -0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>React</h1>
+    <img width="100px" height="100px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/500px-JavaScript-logo.png"/>
+    </>
+  )
+}
+
+export default App
